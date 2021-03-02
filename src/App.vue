8b5929927c3ea4a883bee9421aab45f64258120f
Alvaro <alvaroph@gmail.com>
2021-03-02 20:31:00 +0100

    comienzo de nuevo componente

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Busqueda/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Busqueda from './components/Busqueda.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Busqueda
   }
 }
 </script>
---
+++ src/components/Busqueda.vue
@@ -0,0 +1,41 @@
+<template>
+  <div >
+   <b-button @click=peticionDatos>Consultar</b-button>
+   
+   {{resultado}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Busqueda',
+  props: {
+    resultado:[]
+  },
+  methods:{
+    peticionDatos : function(){
+      this.axios.get('http://labs.iam.cat/~aperezh/BACKvviajes/api.php/records/EXPERIENCIA').then((response) => {
+      this.resultado=response.data.records;
+    })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
